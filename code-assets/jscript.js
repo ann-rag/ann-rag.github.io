715d3967fb1870353b273d69e2ef61cfce21ef45
a15398 <anurag15398@gmail.com>
2020-09-18 10:29:50 +0530

    Added Images to galery

--- code-assets/jscript.js
+++ code-assets/jscript.js
@@ -1,6 +1,11 @@
-var images = [{"image":"images/dragonfly.jpg","label":"","caption":"This is an image"},
-                {"image":"images/moneyplant.png","label":"","caption":"This is an image"},
-                {"image":"images/ant.png","label":"","caption":"This is an image"}];
+var images = [{"image":"images/galery/dragonfly.jpeg","label":"","caption":"A dragon fly. Taken on 12 Sep 2020 @ Mirzapur"},
+                {"image":"images/galery/plant.jpeg","label":"","caption":"A tiny plant while dropping water. Perfect timing :P. Taken on 10 Jun 2020 @ Hyderabad"},
+                {"image":"images/galery/leaf.jpeg","label":"","caption":"A peepal leaf covering the sun. Taken on 11 Nov 2019 @ Varanasi"},
+                {"image":"images/galery/moneyplant.jpg","label":"","caption":"Money Plant. Taken on 5 May 2020 @ Hyderabad"},
+                {"image":"images/galery/starfish.jpeg","label":"","caption":"A live star fish in the sand and water. Taken on 4 Oct 2018 @ Goa"},
+                {"image":"images/galery/ant.jpg","label":"","caption":"An ant sitting on my meal bowl. Taken on 20 Aug 2020 @ Varanasi"},
+                {"image":"images/galery/bullet.jpeg","label":"","caption":"My Royal Enfield Thunderbird 2010 Model. Taken on 15 Feb 2019 @ Hyderabad "},
+                {"image":"images/galery/deep.jpeg","label":"","caption":"A mud lamp (diya) on Diwali 27 Oct 2019 @ Hyderabad"}];
 
 window.onload = function () {
     buildGaleryHTML();
@@ -8,37 +13,42 @@ window.onload = function () {
 var slideIndex = 0;
 
 function plusSlides(n) {
-    showThisSlide(slideIndex += n);
+    showThisSlide(slideIndex+n);
 }
 function buildGaleryHTML(){
-    var galery = document.getElementById("galeryview");
-    var html="";
+    
     var n = images.length;
+    /*var html="";
+    
     for(var i=0;i<n;i++){
         html+='<div class="mySlides"><div class="numbertext">'+(i+1)+' / '+n+'</div>'+
                '<img src="'+images[i].image+'" /><div class="text">'+images[i].caption+'</div></div>';
     }
-    galery.innerHTML=html;
+    var galery = document.getElementById("galeryview");
+    galery.innerHTML=html;*/
 
     var dotcontainer =  document.getElementById("dotcontainer");
-    html="";
+    var html="";
     for(var i=0;i<n;i++){
         html+='<span class="dot" onclick="showThisSlide('+i+')"></span>';
     }
     dotcontainer.innerHTML=html;
     showThisSlide(0);
 }
-function showThisSlide(slideIndex){
-    var slides = document.getElementsByClassName("mySlides");
+function showThisSlide(idx){
+    if (idx >= images.length) {idx = 0}
+    else if (idx < 0) {idx = images.length-1}
     var dots = document.getElementsByClassName("dot");
-    if (slideIndex >= slides.length) {slideIndex = 0}    
-    if (slideIndex < 0) {slideIndex = slides.length-1}
-    for (var i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
     for (var i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex].style.display = "block";  
-    dots[slideIndex].className += " active";
+    dots[idx].className += " active";
+    $(".blur-bg").css("background",'url('+images[idx].image+')');
+    $("#galeryImage").fadeOut(200, function(){
+        $("#galeryImage").attr("src",images[idx].image);
+    });
+    $("#galeryImage").fadeIn(300);
+    $("#numbertext").text((idx+1) +" / "+images.length);
+    $("#caption").text(images[idx].caption);
+    slideIndex = idx;
 }
